d7e7c137244ac0f2e7f975cb968f992195ce6d6e
heiltutanvidare <vusvol@icloud.com>
2020-08-30 15:26:28 +0200

    Feature: Completed basic functionality to display Mars weather in the DOM.
    The Insight sensors are not working, and NASA is on the task

--- js/marsWeather.js
+++ js/marsWeather.js
@@ -1,36 +1,23 @@
-// const key = "?api_key=bTf1g1Q2o8yIUAKBFUNwZFotBpUMxecD5efRgfr6";
-// const weatherURL =
-//     "https://api.nasa.gov/insight_weather/" + key + "&feedtype=json&ver=1.0";
-
-// // Fetch latest Mars weather data
-// function fetchWeather() {
-//     return fetch(weatherURL)
-//         .then((res) => res.json())
-//         .then((data) => {
-//             const { sol_keys, validity_checks, ...solData } = data;
-//             return Object.entries(solData).map(([sol, data]) => {
-//                 return {
-//                     sol: sol,
-//                     maxTemp: data.AT.mx,
-//                     minTemp: data.AT.mn,
-//                     maxSpeed: data.HWS.mx,
-//                     minSpeed: data.HWS.mn,
-//                     date: new Date(data.First_UTC),
-//                 };
-//             });
-//         });
-// }
-
 async function fetchWeather() {
+    // My NASA API key
     const key = "?api_key=bTf1g1Q2o8yIUAKBFUNwZFotBpUMxecD5efRgfr6";
+
+    // Using DEMO key while NASA is fixing the Insight sensors
+    const demoKey = "?api_key=DEMO_KEY";
+
+    // API Base url with added key and feed type/version
     const url =
         "https://api.nasa.gov/insight_weather/" +
         key +
         "&feedtype=json&ver=1.0";
+
+    // Fetching response from the API and calling the displayWeather function
     try {
         const response = await fetch(url);
         const json = await response.json();
         displayWeather(json);
+
+        // Cathing any errors and displaying error message
     } catch (error) {
         console.error(error);
 
@@ -43,13 +30,64 @@ async function fetchWeather() {
                 I'm sorry Dave, I'm afraid I can't do that.
             </p>
         `;
-        // loader.style.display = "none";
     }
 }
 
+// Calling the function to fetch the weather
 fetchWeather();
 
+// Function to display the weather in the DOM
 function displayWeather(json) {
-    const keys = Object.entries(json);
-    console.log(keys);
+    console.dir(json);
+
+    // Get all sols from the API response
+    const sols = json.sol_keys;
+
+    // Declare the HTML variable
+    let html = "";
+
+    sols.forEach((sol) => {
+        let weather = json[sol];
+        console.log(weather);
+
+        // Get the corresponding earth date
+        const earthDate = new Date(weather.First_UTC).toLocaleDateString(
+            undefined,
+            {
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+            }
+        );
+
+        // Get the air temperatures
+        const maxTemp = weather.AT.mx;
+        const minTemp = weather.AT.mn;
+
+        // Get the wind speed
+        const maxWind = weather.HWS.mx;
+        const minWind = weather.HWS.mn;
+
+        // Create the html for displaying the weather data
+        html += `
+                <p class="weather__date">Sol ${sol} | ${earthDate}</p>
+                <p class="weather__heading">Air Temperature</p>
+                <p class="weather__details">
+                    Low: ${minTemp}&deg; C | High: ${maxTemp}&deg; C
+                </p>
+                <p class="weather__heading">Wind speed</p>
+                <p class="weather__details">Min: ${maxWind} m/s | Max: ${maxWind} m/s</p>
+                <a href="#" class="btn btn--primary">Mars weather</a>
+        `;
+    });
+
+    // get the container to display the weather
+    const container = document.querySelector(".weather-container");
+
+    // Apply the HTML to the container
+    container.innerHTML = html;
+
+    // Remove the loader
+    const loader = document.querySelector(".weather-loader");
+    loader.style.display = "none";
 }
