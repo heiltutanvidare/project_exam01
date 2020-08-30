8bf5d54f36a5ed0d41175f4de5ed1e981d0bc1f5
heiltutanvidare <vusvol@icloud.com>
2020-08-30 18:19:02 +0200

    Feature: Added logic to fetch APOD and display it in the DOM

---
+++ js/apod.js
@@ -0,0 +1,59 @@
+// Fetch Astronomy picture of the day
+async function fetchAPOD() {
+    try {
+        // Base APOD URL
+        const url = "https://api.nasa.gov/planetary/apod";
+
+        // My NASA API key
+        const key = "?api_key=bTf1g1Q2o8yIUAKBFUNwZFotBpUMxecD5efRgfr6";
+        const response = await fetch(url + key);
+        const apod = await response.json();
+        displayAPOD(apod);
+    } catch (error) {
+        console.error(error);
+
+        const errorContainer = document.querySelector(".apod-error-container");
+        errorContainer.innerHTML = `
+            <p class="error__message">
+                Will Robinson! Danger!
+            </p>
+        `;
+    }
+}
+
+// Call the fetch apod function
+fetchAPOD();
+
+function displayAPOD(apod) {
+    console.log(apod);
+
+    // Get the image elements from the API result
+    const img = apod.url;
+    const title = apod.title;
+    const date = new Date(apod.date).toDateString();
+    const caption = apod.explanation;
+
+    // Define the HTML for the APOD
+    const html = `
+            <div class="apod__img-container">
+                <img
+                    src="${img}"
+                    alt="${title}"
+                    class="apod__img"
+                />
+                <div class="apod__bg btn--primary extra-bleed"></div>
+            </div>
+            <p class="apod__date">${date}</p>
+            <h3 class="apod__title">${title}</h3>
+            <p class="apod__caption">${caption}</p>
+            </div>
+    `;
+
+    // Apply the HTML to the container
+    const container = document.querySelector(".apod__container");
+    container.innerHTML = html;
+
+    // Remove the loader
+    const loader = document.querySelector(".apod-loader");
+    loader.style.display = "none";
+}
