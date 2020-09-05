927f1ff561d3597dfc0e70e77fabcbe676d013b4
heiltutanvidare <vusvol@icloud.com>
2020-09-05 14:40:05 +0200

    Style: Reworked the planet cards to work propper on mobile

--- js/solarsystem.js
+++ js/solarsystem.js
@@ -15,44 +15,47 @@ function displayPlanets(planets) {
         const yearLength = planets[i].year_length;
 
         html += `
-                <div class="planet" style="background-image: url(img/photos/solar_system/${img})">
-                    <div class="planet__top">
-                        <div class="planet__heading">
-                            <p class="planet__number">${number}</p>
-                            <p class="planet__tagline">${tagline}</p>
-                            <h2 class="planet__name">${planetName}</h2>
+                <div class="planet">
+                    <div class="planet__text">
+                        <div class="planet__top">
+                            <div class="planet__heading">
+                                <p class="planet__number">${number}</p>
+                                <p class="planet__tagline">${tagline}</p>
+                                <h2 class="planet__name">${planetName}</h2>
+                            </div>
+                            <p class="planet__description">${description}</p>
                         </div>
-                        <p class="planet__description">${description}</p>
-                    </div>
-            
-                    <div class="planet__bottom">
-                        <div class="planet__facts">
-                            <div class="planet__fact">
-                                <p class="planet__fact__type">Planet type</p>
-                                <p class="planet__fact__text">${type}</p>
-                            </div>
-                            <div class="planet__fact">
-                                <p class="planet__fact__type">Moons</p>
-                                <p class="planet__fact__text">${moons}</p>
-                            </div>
-                            <div class="planet__fact">
-                                <p class="planet__fact__type">Equatorial radius</p>
-                                <p class="planet__fact__text">${radius} km</p>
-                            </div>
-                            <div class="planet__fact">
-                                <p class="planet__fact__type">Avg. distance from sun</p>
-                                <p class="planet__fact__text">${distance} km</p>
-                            </div>
-                            <div class="planet__fact">
-                                <p class="planet__fact__type">Length of day</p>
-                                <p class="planet__fact__text">${dayLength}</p>
-                            </div>
-                            <div class="planet__fact">
-                                <p class="planet__fact__type">Length of year</p>
-                                <p class="planet__fact__text">${yearLength} earth days</p>
+                
+                        <div class="planet__bottom">
+                            <div class="planet__facts">
+                                <div class="planet__fact">
+                                    <p class="planet__fact__type">Planet type</p>
+                                    <p class="planet__fact__text">${type}</p>
+                                </div>
+                                <div class="planet__fact">
+                                    <p class="planet__fact__type">Moons</p>
+                                    <p class="planet__fact__text">${moons}</p>
+                                </div>
+                                <div class="planet__fact">
+                                    <p class="planet__fact__type">Equatorial radius</p>
+                                    <p class="planet__fact__text">${radius} km</p>
+                                </div>
+                                <div class="planet__fact">
+                                    <p class="planet__fact__type">Distance from sun</p>
+                                    <p class="planet__fact__text">${distance} km</p>
+                                </div>
+                                <div class="planet__fact">
+                                    <p class="planet__fact__type">Length of day</p>
+                                    <p class="planet__fact__text">${dayLength}</p>
+                                </div>
+                                <div class="planet__fact">
+                                    <p class="planet__fact__type">Length of year</p>
+                                    <p class="planet__fact__text">${yearLength} earth days</p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <img src="img/photos/solar_system/${img}" alt="${planetName}" class="planet__image">
                 </div>
         `;
     }
